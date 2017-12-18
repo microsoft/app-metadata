f1438a1e46377c4b38edbb645969e63a5685ca76
BO KANG <bokan@microsoft.com>
2017-12-18 15:08:07 -0800

    fixed the issue to extract ipa icon files.

--- src/contentIPA.ts
+++ src/contentIPA.ts
@@ -41,7 +41,7 @@ export class IpaContent extends ContentBase {
     private iconSearch(fileList: string[]): string {
         let chosenIcon = null;
         for (const plistIcon of fileList) {
-            if (!chosenIcon && plistIcon.toLowerCase().includes("icon")) {
+            if (!chosenIcon && plistIcon.toLowerCase().includes("png")) {
                 chosenIcon = plistIcon;
             }
             if (plistIcon.includes("3x")) {
