27ed2c27ed913342d9dc0f876ad7a2966745a425
Izik Lisbon <izikl@microsoft.com>
2018-01-10 15:06:21 -0800

    Extract the IPA's executable.

--- src/types.ts
+++ src/types.ts
@@ -31,6 +31,12 @@ export interface IPackageMetadata {
 }
 
 export interface IIPAMetadata extends IPackageMetadata {
+    /** The IPA's provisioning profile. */
     provision: IProvisioningProfile;
+    
+    /** Provisioning profiles for application extensions  */
     appexProvisioningProfiles: IProvisioningProfile[];
+    
+    /** Full path to the binary file in the IPA */
+    executableFullPath: string;
 }
--- test/ipaContentTest.ts
+++ test/ipaContentTest.ts
@@ -2,6 +2,7 @@ import * as should from 'should';
 import { ExtractError } from "../src/extractError";
 import { IpaContent }  from "../src/ipaContent";
 import { WorkingFolder } from '../src/workingFolder';
+import * as fse from 'fs-extra';
 
 describe("IpaContent", () => {
     describe("#read", () => {
@@ -26,41 +27,29 @@ describe("IpaContent", () => {
         context("normal plist collection", () => {
             it("should extract params", async () => {
                 const subject = new IpaContent();
-                subject['workingFolder'] = await WorkingFolder.create();
-                const provision = "Payload 15/SoEntitled.app/Watch/WatchIt.app/embedded.mobileprovision";
-                const unzipPath = "test/assets/adhoc-signed-payload";
-                const plistPath = "Payload 15/Info.plist";
-                await subject.read(unzipPath, [plistPath, provision]);
-                should(subject.displayName).eql("SoEntitled");
-                should(subject.uniqueIdentifier).eql("com.microsoft.SoEntitled");
+                await subject.extract('test/assets/smileToUnlock-adhoc-signed.ipa', await WorkingFolder.create());
+                should(subject.displayName).eql('SmileToUnlockExample');
+                should(subject.uniqueIdentifier).eql('izikl.SmileToUnlock');
                 should(subject.version).eql("1.0");
                 should(subject.buildVersion).eql("1");
-                should(subject.executableName).eql("SoEntitled");
-                should(subject.minimumOsVersion).eql("10.0");
+                should(subject.executableName).eql("SmileToUnlockExample");
+                should(fse.existsSync(subject.executableFullPath)).be.true;
+                should(subject.minimumOsVersion).eql("11.1");
                 should(subject.deviceFamily).eql("iPhone/iPod/iPad");
             });
         });
         context("existing icon", () => {
             it("should extract icon and icon name", async () => {
                 const subject = new IpaContent();
-                subject['workingFolder'] = await WorkingFolder.create();
-                const unzipPath = "test/assets/basketball-payload";
-                const plistPath = "Payload/bouncyhoops.app/Info.plist";;
-                const icon = "Payload/bouncyhoops.app/AppIcon72x72@2x~ipad.png";
-                const provision = "embedded.mobileprovision";
-                await subject.read(unzipPath, [plistPath, icon, provision]);
-                should(subject.iconName).eql("AppIcon72x72@2x~ipad.png"); 
+                await subject.extract('test/assets/smileToUnlock-adhoc-signed.ipa', await WorkingFolder.create());
+                should(subject.iconName).eql("AppIcon76x76@2x~ipad.png"); 
                 should(subject.icon).not.eql(undefined);
             });
         });
         context("non-existent icon", () => {
             it("shouldn't extract icon", async () => {
                 const subject = new IpaContent();
-                subject['workingFolder'] = await WorkingFolder.create();
-                const unzipPath = "test/assets/basketball-payload";
-                const plistPath = "Payload/bouncyhoops.app/Info.plist";;
-                const provision = "embedded.mobileprovision";
-                await subject.read(unzipPath, [plistPath, provision]);
+                await subject.extract('test/assets/smileToUnlock-adhoc-signed-no-icon.ipa', await WorkingFolder.create());
                 should(subject.iconName).eql(undefined); 
                 should(subject.icon).eql(undefined);
             });
@@ -68,17 +57,13 @@ describe("IpaContent", () => {
         context("embedded.mobileprovision", () => {
             it("should extract provisioning profile", async () => {
                 const subject = new IpaContent();
-                subject['workingFolder'] = await WorkingFolder.create();
-                const provision = "Payload 15/SoEntitled.app/Watch/WatchIt.app/embedded.mobileprovision";
-                const unzipPath = "test/assets/adhoc-signed-payload";
-                const plistPath = "Payload 15/Info.plist";
-                await subject.read(unzipPath, [plistPath, provision]);
-                should(subject.provision.teamIdentifier).eql("FYD86LA7RE");
+                await subject.extract('test/assets/smileToUnlock-adhoc-signed.ipa', await WorkingFolder.create());
+                should(subject.provision.teamIdentifier).eql("L47QS6HV2U");
                 should(subject.provision.profileType).eql("adhoc");
-                should(subject.provision.expiredAt).eql(new Date('2018-01-24T01:02:18.000Z'));
-                should(subject.provision.idName).eql("CalabashWildcard");
-                should(subject.provision.UniqueDeviceIdentifierList.length).eql(39);
-                should(subject.provision.mobileProvisionFileContent);
+                should(subject.provision.expiredAt).eql(new Date('2018-11-28T23:46:08.000Z'));
+                should(subject.provision.idName).eql("izikl-SmileToUnlock");
+                should(subject.provision.UniqueDeviceIdentifierList.length).eql(1);
+                should(subject.provision.mobileProvisionFileContent).not.empty;
             });
         });
         context("no provisioning profile", () => {
@@ -87,7 +72,8 @@ describe("IpaContent", () => {
                 const unzipPath = "test/assets/basketball-payload";
                 const plistPath = "Payload/bouncyhoops.app/Info.plist";;
                 const icon = "Payload/bouncyhoops.app/AppIcon72x72@2x~ipad.png";
-                return subject.read(unzipPath, [plistPath, icon]).should.be.rejectedWith(ExtractError);
+                return subject.read(unzipPath, [plistPath, icon])
+                    .should.be.rejectedWith(ExtractError, /cannot find the provisioning profile/g);
             });
         });
         context("invalid provisioning profile path", () => {
